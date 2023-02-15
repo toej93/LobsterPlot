c541169ee6c29a7d779e835716fe3411a297b19d
Jorge Torres <jorge.torresespinosa@yale.edu>
2023-02-15 11:01:24 -0500

    Updated limits for isotopes.
    Thanks to Daniel Mayer for updating some of those.

--- numass.py
+++ numass.py
@@ -251,16 +251,26 @@ def AddExperimentalLimits(IH, NH, xMin, xMax, isotopes=None, yMin=-1, yMax=-1):
         IH.errorbar(A_Xe, mbb_min_Xe, yerr=mbb_max_Xe-mbb_min_Xe-20, lolims=True,  color=arrColor)
         IH.text(A_Xe+3, mbb_min_Xe, '$^{136}$Xe', color=arrColor,fontsize='medium', ha="left", fontweight="book")
 
-    ## Mo limit from CUPID-Mo (2021): https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.126.1
+    ## Mo limit from CUPID-Mo (2022): https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.126.1
     if("Mo" in isotopes):
-        mbb_min_Mo = 300
-        mbb_max_Mo = 500
+        mbb_min_Mo = 280
+        mbb_max_Mo = 490
         A_Mo = 100
         
 #         IH.axhspan(mbb_min_Mo, mbb_max_Mo,lw=0, xmin = 100, xmax = 150, ec='#AA7F39',fill=None, alpha=0.3, hatch='///')
         Moline = IH.hlines(mbb_min_Mo, A_Mo-2, A_Mo+2, color=arrColor, label='$^{100}$Mo limit (CUPID-Mo)', linestyle='-')
         IH.errorbar(A_Mo, mbb_min_Mo, yerr=mbb_max_Mo-mbb_min_Mo-20, lolims=True,  color=arrColor)
         IH.text(A_Mo+3, mbb_min_Mo, '$^{100}$Mo', color=arrColor,fontsize='medium', ha="left", fontweight="book")
+        
+     if("Se" in isotopes):
+        mbb_min_Se = 263
+        mbb_max_Se = 545
+        A_Se = 82
+
+## Se limit from CUPID-0 (2022): https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.129.111801
+        Seline = IH.hlines(mbb_min_Se, A_Se-2, A_Se+2, color=arrColor, label='$^{82}$Se limit (CUPID-0)', linestyle='-')
+        IH.errorbar(A_Se, mbb_min_Se, yerr=mbb_max_Se-mbb_min_Se-20, lolims=True,  color='#2B4970')
+        IH.text(A_Se+3, mbb_min_Se, '$^{82}$Se', color=arrColor,fontsize=isotope_fontsize, ha="left", fontweight="book")
 
 
     ##
